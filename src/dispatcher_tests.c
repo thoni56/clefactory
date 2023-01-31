7e61db3e9570baa0f979c5e5e4fadcc1dee94725
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2023-01-31 13:59:23 +0100

    [unittest] Create temporary file with test data

--- src/dispatcher_tests.c
+++ src/dispatcher_tests.c
@@ -11,19 +11,21 @@ BeforeEach(Dispatcher) {}
 AfterEach(Dispatcher) {}
 
 
-static FILE *create_an_empty_file() {
+static FILE *create_an_empty_file(const char *input) {
     char temp_file_template[] = "/tmp/temp_file.XXXXXX";
     int fd = mkstemp(temp_file_template);
     if (fd == -1) {
         perror("Error creating temporary file");
         exit(EXIT_FAILURE);
     }
+    FILE *output = fdopen(fd, "w");
+    fprintf(output, "%s", input);
     return fdopen(fd, "r");
 }
 
 
 Ensure(Dispatcher, will_exit_immediately_on_empty_input) {
-	FILE *channel = create_an_empty_file();
+	FILE *channel = create_an_empty_file("");
     dispatch_commands(channel);
     assert_that(true);
 }
