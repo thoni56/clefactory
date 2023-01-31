610e31ede0bf9eb7588b41a8387292c85f2eb33f
Thomas Nilefalk <thomas@nilefalk.se>
2023-01-31 22:48:19 +0100

    [build] Trying another install method for Cgreen

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,5 +1,5 @@
 wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1_amd64.deb
-sudo apt-get install ./cgreen-1.6.1_amd64.deb
+sudo apt install ./cgreen-1.6.1_amd64.deb
 lcov --version
 gcov --version
 gem install coveralls-lcov
