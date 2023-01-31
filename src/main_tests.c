f82a70fdbad3a9b016caf57820f685baa2ca49fc
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2023-01-31 10:21:42 +0100

    [build] Initial commit with one running unittest

---
+++ src/main_tests.c
@@ -0,0 +1,7 @@
+#include <cgreen/cgreen.h>
+
+Describe(Main);
+BeforeEach(Main) {}
+AfterEach(Main) {}
+
+Ensure(Main, can_run_an_empty_unittest) {}
---
+++ src/sources.mk
@@ -0,0 +1,14 @@
+MODULES =  main
+
+OBJDIR = .objects
+SRCS = ${MODULES:=.c}
+OBJS = $(addprefix $(OBJDIR)/,${SRCS:.c=.o})
+DEPS = $(addprefix $(OBJDIR)/,${SRCS:.c=.d})
+
+$(OBJDIR)/%.o: %.c | $(OBJDIR)
+	$(CC) $(CFLAGS) $(OUTPUT_OPTION) -MMD -MP -c $<
+
+$(OBJDIR):
+	-mkdir $(OBJDIR)
+
+-include $(OBJDIR)/*.d
