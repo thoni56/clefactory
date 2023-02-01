47c831664196e37ffab9e4199ce381d306499e0d
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-01 01:40:28 +0100

    [build] After fixing the cgreen .deb go for 1.6.1 and hope it works

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,5 +1,5 @@
-wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.0/cgreen-1.6.0_amd64.deb
-sudo apt install ./cgreen-1.6.0_amd64.deb
+wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-amd64.deb
+sudo dpkg -i ./cgreen-1.6.1-amd64.deb
 cgreen-runner --version
 find /usr -name cgreen.h
 lcov --version
