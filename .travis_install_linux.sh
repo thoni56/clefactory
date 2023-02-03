cfe652929fff41df888a68386af6043e84fa7286
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-03 16:37:10 +0100

    [travis] Update install of cgreen with new package name for Ubuntu

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,7 +1,6 @@
-wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-amd64.deb
-sudo dpkg -i ./cgreen-1.6.1-amd64.deb
+wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-x86_64-linux-gnu.deb
+sudo dpkg -i ./cgreen-1.6.1-x86_64-linux-gnu.deb
 cgreen-runner --version
-find /usr -name cgreen.h
 lcov --version
 gcov --version
 gem install coveralls-lcov
