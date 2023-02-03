a7ff7ca871d74d621c291d99467766ac5165d3d1
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2023-02-03 14:20:36 +0100

    [cli] Add prompt and a '?' command

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dispatcher.h"
 #include "exports.h"
-#include "indexer.h"
+//#include "indexer.h"
 
 #define MAX_LINE_LENGTH 3000
 
@@ -15,16 +16,24 @@ static int about_handler(const char *arguments[]) {
 static DispatchTable dispatchTable[] = {
     {"about", about_handler},
     {"exports", exports_handler},
-    {"indexer", indexer_handler}
+    {"indexer", indexer_handler},
+    {"", NULL}
 };
 
 int main(int argc, char *argv[]) {
     char command[MAX_LINE_LENGTH];
+    fprintf(stdout, "> ");
     while (fgets(command, sizeof(command), stdin) != NULL) {
-        int error_code = dispatch_command(command, dispatchTable);
-        if (error_code != 0) {
-            return error_code;
+        if (strncmp(command, "?", 1) == 0)
+            for (DispatchTable *t = dispatchTable; t->handler != NULL; t++)
+                fprintf(stderr, "%s\n", t->command);
+        else {
+            int error_code = dispatch_command(command, dispatchTable);
+            if (error_code != 0) {
+                return error_code;
+            }
         }
+        fprintf(stdout, "> ");
     }
     return EXIT_SUCCESS;
 }
