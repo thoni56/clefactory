0852d1c90f6f16bf51e00f94a4edf4992c1565d0
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-06 19:12:25 +0100

    [build] Use apt-installable libclang-15 on Ubuntus

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -7,7 +7,8 @@ lcov --version
 gcov --version
 gem install coveralls-lcov
 # libclang
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-tar xf clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-mv clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04 /opt
-ls /opt
+#wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+#tar xf clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+#mv clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04 /opt
+#ls /opt
+apt install libclang-15-dev
