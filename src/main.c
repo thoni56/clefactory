87e133de37c815eee7d3436c47a7d02157d6970c
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-09 22:47:41 +0100

    [dispatch] Introduce a help_handler that automatically prints all commands

--- src/main.c
+++ src/main.c
@@ -18,30 +18,43 @@ static int about_handler(CXIndex index, const char *arguments[]) {
 
 static const char *about_help(void) { return ""; }
 
+static int help_handler(CXIndex index, const char *arguments[]);
+
 static DispatchTable dispatchTable[] = {
-    {"about", about_handler, about_help},
-    {"exports", exports_handler, exports_help},
-    {"includes", includes_handler, includes_help},
-    {"indexer", indexer_handler, indexer_help},
+    {"about",      about_handler,      about_help     },
+    {"exports",    exports_handler,    exports_help   },
+    {"includes",   includes_handler,   includes_help  },
+    {"indexer",    indexer_handler,    indexer_help   },
     {"references", references_handler, references_help},
-    {"", NULL, NULL}
+    {"?",          help_handler,       NULL           }, /* NOTE: Help will stop at "?" */
+    {"help",       help_handler,       NULL           },
+    {"",           NULL,               NULL           }
 };
+
+static int help_handler(CXIndex index, const char *arguments[]) {
+    for (DispatchTable *t = dispatchTable; strcmp(t->command, "?") != 0; t++)
+        fprintf(stdout, "%s %s\n", t->command, t->help());
+    return EXIT_SUCCESS;
+}
 
 int main(int argc, char *argv[]) {
     char command[MAX_LINE_LENGTH];
 
+    // TODO: create a compilation database object using clang_CompilationDatabase_fromDirectory()
+    // TODO: use that to get compile flags and options for a specific translation unit
     CXIndex index = createIndex(0, 0);
+
+    // TODO: create a table of filenames of all translation units,
+    // all their dependent included files and their latest
+    // modification time ...
 
     fprintf(stdout, "> ");
     while (fgets(command, sizeof(command), stdin) != NULL) {
-        if (strncmp(command, "?", 1) == 0)
-            for (DispatchTable *t = dispatchTable; t->handler != NULL; t++)
-                fprintf(stdout, "%s %s\n", t->command, t->help());
-        else {
-            int error_code = dispatch_command(index, command, dispatchTable);
-            if (error_code != 0) {
-                fprintf(stderr, "Error: %d\n", error_code);
-            }
+        // TODO: ... so that we can refresh the index if needed,
+        // before doing the operation
+        int error_code = dispatch_command(index, command, dispatchTable);
+        if (error_code != 0) {
+            fprintf(stderr, "Error: %d\n", error_code);
         }
         fprintf(stdout, "> ");
     }
