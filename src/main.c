c98e761fb3caeac285686374cad28ee1280c8cf1
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2023-02-13 11:16:32 +0100

    [tidy] fileTable is not UNUSED any more

--- src/main.c
+++ src/main.c
@@ -51,7 +51,6 @@ int main(int argc, char *argv[]) {
     // TODO: create a compilation database object using clang_CompilationDatabase_fromDirectory()
     // TODO: use that to get compile flags and options for a specific translation unit
     FileTable fileTable = getTranslationUnitsFromCurrentDirectory();
-    UNUSED fileTable;
 
     CXIndex index = createIndex(0, 0);
 
