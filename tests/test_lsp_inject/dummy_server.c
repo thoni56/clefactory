89d3f14d4b1d88fbe66af78b2239772ea2542ecc
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-19 13:53:06 +0100

    [misc] Mucking around with forks and server_handler...

--- tests/test_lsp_inject/dummy_server.c
+++ tests/test_lsp_inject/dummy_server.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <cjson/cJSON.h>
+
+
 int main(int argc, char **argv) {
     char input[1000];
     const char exit_request[] = "{\"jsonrpc\":\"2.0\",\"id\": 1,\"method\": \"exit\"}";
     while (fgets(input, sizeof(input), stdin)) {
-        fprintf(stderr, "server received: '%s'\n", input);
-        if (strcmp(input, exit_request) == 0)
-            break;
+        cJSON *root = cJSON_Parse(input);
+        cJSON *method = cJSON_GetObjectItem(root, "method");
+        if (method != NULL) {
+            if (strcmp(method->valuestring, "initialize") == 0) {
+                printf("Received an 'initialize' message\n");
+            } else if (strcmp(method->valuestring, "shutdown") == 0) {
+                printf("Received a 'shutdown' message\n");
+            } else if (strcmp(method->valuestring, "exit") == 0) {
+                printf("Received an 'exit' message\n");
+                return EXIT_SUCCESS;
+            } else {
+                printf("Received an unknown message with method '%s'\n", method->valuestring);
+            }
+        } else {
+            printf("Received an invalid JSON-RPC message\n");
+        }
+        cJSON_Delete(root);
     }
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
 }
