9887bdef3db8b7b3ca3a2f5f6fd3c6802170b252
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2023-02-27 17:19:33 +0100

    [client] First version of unittest

--- src/client_handler_tests.c
+++ src/client_handler_tests.c
@@ -17,4 +17,13 @@ AfterEach(ClientHandler) {}
 
 
 Ensure(ClientHandler, can_compile_with_an_empty_test) {
+    char *content_length_header = "Content-Length: 43\r\n";
+    char *delimiter = "\r\n";
+
+    expect(readLine, will_set_contents_of_parameter(buffer, content_length_header, strlen(content_length_header)),
+           will_return(content_length_header));
+    expect(readLine, will_set_contents_of_parameter(buffer, delimiter, strlen(delimiter)),
+           will_return(content_length_header));
+
+    handle_client_request(42);
 }
