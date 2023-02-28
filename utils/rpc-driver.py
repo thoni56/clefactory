bfbf374f7d8b6fce2a22e44020c56c884f6c856b
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-28 16:51:56 +0100

    [utils] Add Content-type in the header sent by rpc-driver

--- utils/rpc-driver.py
+++ utils/rpc-driver.py
@@ -12,6 +12,6 @@ with open(sys.argv[1], 'r') as input_file:
         # Calculate the length of the JSON string in bytes
         content_length = len(line.encode('utf-8'))
         # Create the transport message as a string
-        transport_message = f'Content-Length: {content_length}\r\n\r\n{line}\r\n\r\n'
+        transport_message = f'Content-Length: {content_length}\r\nContent-type: application/vscode-jsonrpc;charset=utf-8\r\n\r\n{line}\r\n\r\n'
         # Write the transport message to stdout
         sys.stdout.write(transport_message)
