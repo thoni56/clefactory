997e9772a1ff68e37cb01a27877038795023e754
Thomas Nilefalk <thomas@nilefalk.se>
2023-03-01 08:30:51 +0100

    [utils] Restructure rpc-driver

--- utils/rpc-driver.py
+++ utils/rpc-driver.py
@@ -2,16 +2,25 @@
 
 import json
 import sys
+import time
 
 # Open the input file
 with open(sys.argv[1], 'r') as input_file:
     # Iterate over each line in the input file
+    time.sleep(1)
     for line in input_file:
         # Strip whitespace from the line
         line = line.strip()
+
         # Calculate the length of the JSON string in bytes
-        content_length = len(line.encode('utf-8'))
-        # Create the transport message as a string
-        transport_message = f'Content-Length: {content_length}\r\nContent-type: application/vscode-jsonrpc;charset=utf-8\r\n\r\n{line}\r\n\r\n'
-        # Write the transport message to stdout
-        sys.stdout.write(transport_message)
+        content_length = len(line)
+
+        # Send header
+        sys.stdout.write('Content-Length: %d\r\n' % content_length)
+        sys.stdout.write('Content-Type: application/vscode-jsonrpc;charset=utf-8\r\n')
+        sys.stdout.write('\r\n')
+
+        # Send payload with delimiter
+        sys.stdout.write('%s\r\n' % line)
+        sys.stdout.write('\r\n')
+        sys.stdout.flush()
