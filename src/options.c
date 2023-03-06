b921aed34d8bc3ab9938a3b670f5f4c27065cbeb
Thomas Nilefalk <thomas@nilefalk.se>
2023-03-06 18:38:21 +0100

    [lsp] Using default clangd if none given

--- src/options.c
+++ src/options.c
@@ -13,9 +13,12 @@ ResultCode decode_options(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--cli") == 0) {
             options.mode = CLI_MODE;
-        } else if (strncmp("--lsp=", argv[i], 6) == 0) {
-            options.lsp_server_path = argv[i] + 6;
+        } else if (strncmp("--lsp", argv[i], 5) == 0) {
             options.mode = LSP_MODE;
+            if (strlen(argv[i]) > 5 && argv[i][5] == '=')
+                options.lsp_server_path = argv[i] + 6;
+            else
+                options.lsp_server_path = "clangd-15";
         } else if (strncmp("--log=", argv[i], 6) == 0) {
             options.logfile_path = argv[i] + 6;
         } else if (strcmp("--trace", argv[i]) == 0) {
